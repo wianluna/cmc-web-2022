f0f8ab16c735f827ee97d6e599dc3307e72719fb
Anna Chistyakova <ann24111@mail.ru>
2022-03-07 21:52:44 +0300

    Add sql scripts

---
+++ sql_scripts/create_db.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS Physical_Properties;
+DROP TABLE IF EXISTS Specimens_Composition;
+DROP TABLE IF EXISTS Mineral_Specimens;
+DROP TABLE IF EXISTS Expeditions;
+DROP TABLE IF EXISTS Mineral_Species;
+DROP TABLE IF EXISTS Classification;
+
+DROP OWNED BY web_server_user;
+DROP USER IF EXISTS web_server_user;
+
+DROP DATABASE IF EXISTS web_server_db;
+--CREATE DATABASE web_server_db;
+
+CREATE USER web_server_user WITH ENCRYPTED PASSWORD '1';
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO web_server_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO web_server_user;
+
+--CONNECT TO web_server_db;
+SET ROLE web_server_user;
---
+++ sql_scripts/create_tables.sql
@@ -0,0 +1,61 @@
+SET DATESTYLE TO "DMY";
+
+DROP TABLE IF EXISTS Physical_Properties;
+DROP TABLE IF EXISTS Specimens_Composition;
+DROP TABLE IF EXISTS Mineral_Specimens;
+DROP TABLE IF EXISTS Expeditions;
+DROP TABLE IF EXISTS Mineral_Species;
+DROP TABLE IF EXISTS Classification;
+
+CREATE TABLE Classification (
+    class_id SERIAL,
+    type text,
+    class text,
+    subclass text,
+    PRIMARY KEY (class_id)
+);
+
+CREATE TABLE Mineral_Species (
+    species_id SERIAL,
+    species_name text NOT NULL,
+    chemical_formula text DEFAULT NULL,
+    origin text DEFAULT NULL, -- осадочное, вулканическое, метаморфическое
+    class_id integer REFERENCES Classification ON DELETE SET NULL ON UPDATE CASCADE,
+    PRIMARY KEY (species_id)
+);
+
+CREATE TABLE Physical_Properties (
+    species_id integer REFERENCES Mineral_Species ON DELETE CASCADE ON UPDATE CASCADE,
+    lattice_type text NOT NULL,
+    hardness float NOT NULL,
+    lustre text NOT NULL,
+    color text NOT NULL,
+    magnetism text NOT NULL,
+    PRIMARY KEY (species_id)
+);
+
+CREATE TABLE Expeditions (
+    expedition_id SERIAL,
+    date_start timestamp,
+    date_end timestamp,
+    members text[],
+    description text,
+    PRIMARY KEY (expedition_id)
+);
+
+CREATE TABLE Mineral_Specimens (
+    specimen_id SERIAL,
+    possible_origin text DEFAULT NULL,
+    location_of_detection jsonb,
+    source text,
+    expedition_id integer REFERENCES Expeditions ON DELETE RESTRICT,
+    PRIMARY KEY (specimen_id)
+);
+
+CREATE TABLE Specimens_Composition (
+    specimen_id integer NOT NULL references Mineral_Specimens,
+    species_id integer NOT NULL references Mineral_Species,
+    inclusion_type text,
+    percentage float,
+    PRIMARY KEY (specimen_id, species_id)
+);
